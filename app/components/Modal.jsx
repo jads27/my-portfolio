dc309ddb476ec5d0d23304a45ee58cbf6e214e58
jads27 <jalvesdsilva27@gmail.com>
2025-03-30 17:29:20 +0200

    Add preload and fallback image

--- app/components/Modal.jsx
+++ app/components/Modal.jsx
@@ -4,16 +4,33 @@ import FocusLock from "react-focus-lock"
 import Image from "next/image"
 import { FaXmark } from "react-icons/fa6"
 
-export default function Modal({ isVisible, onClose, videoMp4, videoWebm , alt, title, details, stack, redirectButton }) {
+export default function Modal({ isVisible, onClose, imagePreview, videoMp4, videoWebm, alt, title, details, stack, redirectButton }) {
     if ( !isVisible ) return null
 
+    const [videoLoaded, setVideoLoaded] = useState(false)
     const videoRef = useRef(null)
 
     useEffect(() => {
-        if (isVisible && videoRef.current) {
+        const video = document.createElement("video")
+        if (videoWebm) video.src = videoWebm
+        else if (videoMp4) video.src = videoMp4
+        video.preload = "auto"
+
+        video.onloadeddata = () => {
+            setVideoLoaded(true)
+        }
+
+        return () => {
+            video.pause()
+            video.src = ""
+        }
+    }, [videoWebm, videoMp4])
+
+    useEffect(() => {
+        if (isVisible && videoRef.current && videoLoaded) {
             videoRef.current.play()
         }
-    }, [isVisible])
+    }, [isVisible, videoLoaded])
 
     const handleModalClose = () => {
         onClose()
@@ -71,22 +88,32 @@ export default function Modal({ isVisible, onClose, videoMp4, videoWebm , alt, t
                         <div className="flex flex-col-reverse justify-between my-auto mx-auto w-full gap-8 max-w-6xl px-4 my-34 mt-20 text-center md:flex-row md:text-start md:mt-auto dark:text-[#F5F5F5]">
                             <div className="flex flex-col-reverse w-full md:max-w-lg md:flex-col">
                                 {videoMp4 || videoWebm ? (
-                                    <video ref={videoRef} className="rounded-xl" preload="metadata" autoplay muted>
-                                        {videoWebm && <source src={videoWebm} type="video/webm" />}
-                                        {videoMp4 && <source src={videoMp4} type="video/mp4" />}
-                                        Votre navigateur ne supporte pas la lecture des vidéos.
-                                    </video>
-                                ) : (
-                                    <Image
-                                        className="rounded-xl"
-                                        src={imagePreview}
-                                        alt={alt}
-                                        width={204}
-                                        height={164}
-                                        layout="responsive"
-                                        priority
-                                    />
-                                )}   
+                                    !videoLoaded ? (
+                                        <Image
+                                            className="rounded-xl" 
+                                            src={imagePreview}
+                                            alt={alt}
+                                            width={204}
+                                            height={164}
+                                            layout="responsive"
+                                            priority
+                                        />
+                                    ) : (   <video ref={videoRef} className="rounded-xl" preload="auto" autoplay muted>
+                                                {videoWebm && <source src={videoWebm} type="video/webm" />}
+                                                {videoMp4 && <source src={videoMp4} type="video/mp4" />}
+                                            </video>
+                                        )
+                                    ) : (
+                                        <Image
+                                            className="rounded-xl"
+                                            src={imagePreview}
+                                            alt={alt}
+                                            width={204}
+                                            height={164}
+                                            layout="responsive"
+                                            priority
+                                        />
+                                    )}   
                                 <div className="flex justify-between gap-12 mb-8 text-center text-xl font-bold md:mb-0 md:mt-8" dangerouslySetInnerHTML={{ __html: redirectButton }} />
                             </div>
                             <div className="space-y-5">
